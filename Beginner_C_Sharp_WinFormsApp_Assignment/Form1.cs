59f32bacdba426eed1b5068cd0e51033ad87b28d
いくユフラン <ikuyuhuran@gmail.com>
2023-05-24 12:17:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Beginner_C_Sharp_WinFormsApp_Assignment/Form1.cs
@@ -0,0 +1,86 @@
+namespace Beginner_C_Sharp_WinFormsApp_Assignment
+{
+
+    /// <summary>
+    /// 題解決型授業
+    /// </summary>
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        /*
+         * 【デバッグをしてみよう】
+         * 
+         * 問題のあるソースコードを配布しますので、これを解決してください。
+         * ※一見問題なさそうなコードもあります
+         * 
+         * また、いくつか実装してほしい内容も記述されていますので、こちらも解決してください。
+         */
+
+        /// <summary>
+        /// 「7 + 3 = 10」と表示させたい。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int num1 = 7;
+            int num2 = 3;
+
+            label1.Text = num1 + " + " + num2 + " = ";
+
+        }
+
+        /// <summary>
+        /// num1 ÷ num2 の結果を表示させたい。
+        /// エラーで止まってほしくない。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox2.Text);
+            int result = 0;
+
+            result = num1 / num2;
+
+            label2.Text = result;
+
+        }
+
+        /*
+         * 文字列を6つ取得し、5W1hの文章を作成するプログラムコードを書いてください。
+         * 
+         * e.g.)
+         * 　入力：When（いつ）、Where（どこで）、Who（誰が）、What（何を）、Why（なぜ）、How（どのように）
+         * 　出力：「先週、私は学校から課題を与えられたため、自宅でPCを使ってその課題を完成させた。」
+         */
+
+        /*
+         * 1～100のランダムな整数を2つ使い、四則演算の結果を表示するプログラムコードを書いてください。
+         * 
+         * ランダムな整数の発生方法：
+         *    Random random = new Random(); ←ランダムオブジェクトを生成
+         *    int number = random.Next(0, 10); ←0～10の整数が呼ばれるたびに発生
+         * 
+         * e.g.)
+         * 　出力結果：「32 ＋ 78 ＝ 110」「85 － 24 ＝ 61」「5 × 61 ＝ 305」「40 ÷ 2 ＝ 20」
+         * 
+         * cf.)
+         * 『C#のRandomの使い方｜Randomクラスの機能と乱数配列の生成』
+         * https://www.fenet.jp/dotnet/column/language/8212/
+         * 
+         */
+
+    }
+
+}
